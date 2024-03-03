48a05b84286a44170b10e5285b7391ee3c848920
James Jacobsen <edjcjacobsen@gmail.com>
2024-03-02 22:02:51 -0600

    tailwind init

---
+++ fitness_app/frontend/src/components/landingPage/LandingPage.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LandingPage = () => {
+  return (
+    <div class="bg-slate-500">
+      <h1 class="text-3xl">Dayyum!</h1>
+    </div>
+  );
+};
+
+export default LandingPage;
