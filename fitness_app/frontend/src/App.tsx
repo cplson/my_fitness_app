874a9dc2e4a91df2ab6d366c6cd7d08c79e7e920
James Jacobsen <edjcjacobsen@gmail.com>
2024-03-14 23:43:39 -0500

    added navigation for the landingPage button to either /login or /user using react-router-dom

--- fitness_app/frontend/src/App.tsx
+++ fitness_app/frontend/src/App.tsx
@@ -1,13 +1,31 @@
-import "./App.css";
 import React from "react";
 import LandingPage from "./components/landingPage/LandingPage";
+import {
+  RouterProvider,
+  createBrowserRouter,
+} from "../node_modules/react-router-dom/dist/index";
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <LandingPage />,
+    },
+    {
+      path: "/login",
+      element: <h1>login</h1>,
+    },
+    {
+      path: "/user",
+      element: <h1>user</h1>,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <div className="bg-stone-200 h-screen w-full">
-        <LandingPage />
-      </div>
+    <div className="bg-stone-200 h-screen w-full">
+      <React.StrictMode>
+        <RouterProvider router={router} />
+      </React.StrictMode>
     </div>
   );
 }
--- fitness_app/frontend/src/components/landingPage/LandingPage.jsx
+++ fitness_app/frontend/src/components/landingPage/LandingPage.jsx
@@ -1,10 +1,28 @@
 import React from "react";
-
+import {
+  redirect,
+  useNavigate,
+} from "../../../node_modules/react-router-dom/dist/index";
+const user = "phil";
 const LandingPage = () => {
+  const navigate = useNavigate();
   return (
-    <div class="h-full w-full">
-      <div class="w-3/5 m-auto">
-        <h1 class="text-3xl bg-blue-300">Dayyum!</h1>
+    <div className="h-full w-full flex">
+      <div className="m-auto flex flex-col items-center space-y-10">
+        <h1 className="text-3xl text-justify">Track calories your way!</h1>
+        <button
+          onClick={() => {
+            console.log("inside event handler");
+            if (user) {
+              console.log("inside user");
+              navigate("/user");
+            }
+            redirect("/login");
+          }}
+          className="border-0 border-solid border-black text-2xl font-bold text-zinc-50 py-2 px-4 rounded bg-blue-700"
+        >
+          Get Started
+        </button>
       </div>
     </div>
   );
